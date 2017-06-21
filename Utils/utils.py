f824a103bb25d85eae23308d388e476531ac06cd
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-21 12:21:29 -0500

    Style changes in 'Utils.utils' module

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,14 +1,15 @@
-#Printint Error File
+# Printint Error File
 from __future__ import print_function
 import sys
-
-def eprint(*args,**kwargs):
-  print(*args, file = sys.stderr, **kwargs)
-
 from .message import MessageList
 
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 
 def read_url_from_file(temp_file):
     str, optional = read_text_from_file(temp_file)
@@ -40,9 +41,9 @@ def read_text_from_file(temp_file):
     # parse text
     try:
         value = data[1]
-    except:
+    except IndexError:
         return None, False
-    
+
     optional = value.startswith('optional')
     if optional:
         value = value[len('optional'):]
@@ -52,102 +53,105 @@ def read_text_from_file(temp_file):
 
     return value, optional
 
-    
 
 def read_int_from_file(temp_file):
-    str, optional = read_text_from_file(temp_file)
-    
-    if str is None:
-        return None, optional
-    
-    try:
-        return int(str), optional
-    except:
+    s, optional = read_text_from_file(temp_file)
+
+    if s is None:
         return None, optional
 
+    try:
+        return int(s), optional
+    except ValueError:
+        return None, optional
+
+
 def validate_tag_format(tag):
-  tag_parts = tag.split()
-  if len(tag_parts) != 2:
-    return False
-  else:
-    index = tag_parts[0]
-    return index.isdigit() or index == '*'
+    tag_parts = tag.split()
+    if len(tag_parts) != 2:
+        return False
+    else:
+        index = tag_parts[0]
+        return index.isdigit() or index == '*'
 
 
 def validate_dictionary_format(dictionary):
-  try:
-    dictionary = eval(dictionary)
-  except:
-    return False
+    try:
+        dictionary = eval(dictionary)
+    except:
+        return False
 
-  return isinstance(dictionary, dict)
+    return isinstance(dictionary, dict)
+
 
 def validate_attribute_format(attr):
-  return attr.isalpha() or attr == ""
+    return attr.isalpha() or attr == ""
 
 
 def validate_string_source(source):
 
-  if source is "":
-    return False
+    if source is "":
+        return False
 
-  nlvls = len(source.split('->'))
-  if (nlvls == 1):
-    nparts = len(source.split('\\'))
-    if nparts == 1:
-      return True
+    nlvls = len(source.split('->'))
+    if (nlvls == 1):
+        nparts = len(source.split('\\'))
+        if nparts == 1:
+            return True
+        else:
+            return False
     else:
-      return False
-  else:
-    return False
+        return False
 
-def read_source_from_string(data, main_list = MessageList()):
-  if data is None:
-      return None
-      
-  if validate_string_source(data):
-    try:  
-      list_data = eval(data)
-      if type(list_data) is list:
-        main_list.add_msg("Using list data from template: " + str(list_data),MessageList.INF)
-        return list_data
-    except:
-      return None
-        
-  else:
-    levels = data.split('->')
-    for level in levels:
-      parts = level.split('\\')
-      if (len(parts)!=3):
+
+def read_source_from_string(data, main_list=MessageList()):
+    if data is None:
         return None
-      else:
-        tag = parts[0]
-        if not validate_tag_format(tag):
-          return None
 
-        dicc = parts[1]
-        if not validate_dictionary_format(dicc):
-          return None
+    if validate_string_source(data):
+        try:
+            list_data = eval(data)
+            if type(list_data) is list:
+                main_list.add_msg(
+                    "Using list data from template: " + str(list_data), MessageList.INF)
+                return list_data
+        except:
+            return None
 
-        attr = parts[2]
-        if not validate_attribute_format(attr):
-          return None
-    return data
+    else:
+        levels = data.split('->')
+        for level in levels:
+            parts = level.split('\\')
+            if (len(parts) != 3):
+                return None
+            else:
+                tag = parts[0]
+                if not validate_tag_format(tag):
+                    return None
+
+                dicc = parts[1]
+                if not validate_dictionary_format(dicc):
+                    return None
+
+                attr = parts[2]
+                if not validate_attribute_format(attr):
+                    return None
+        return data
 
 
 def read_source_from_file(temp_file):
-  value, optional = read_text_from_file(temp_file)
-  if value is None:
-    return None, optional
-  else:
-    return read_source_from_string(value), optional
-  
+    value, optional = read_text_from_file(temp_file)
+    if value is None:
+        return None, optional
+    else:
+        return read_source_from_string(value), optional
 
 
 def is_blank(mystring):
-  if mystring and mystring.strip():
-      return False
-  return True
-  
+    if mystring and mystring.strip():
+        return False
+    return True
+
+
 def clean_whitespaces(text):
-  return ' '.join(text.split())
+    return ' '.join(text.split())
